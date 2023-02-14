b544c6491695938c3f52f89482fcf5a4eb1faa2f
darvinmarte <119225399+darvinmarte@users.noreply.github.com>
2023-02-13 19:54:31 -0500

    Added start function and hidden features.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -20,12 +20,13 @@
 
 
 // Selects element by id
-var startbutton = document.getElementById('startbutton')
+var startButton = document.getElementById('start-quiz')
 var secondsLeftEl = document.getElementById('secondsLeft')
-var strings = document.getElementById('strings')
-var booleans = document.getElementById('booleans')
-var alerts = document.getElementById('alerts')
-
+var containerEl = document.getElementById('container')
+var questionsEl = document.getElementById('questions')
+var titleEl = document.getElementById('title')
+var optionsEl = document.getElementById('options')
+var timerInterval;
 
 // global question number variable
 var questionNumber = 0;
@@ -60,7 +61,7 @@ var questions = [
       "all of the above"
     ],
     correctAnswer: "numbers and strings"
-  }
+  },
   {
     questionText: "String values must be enclosed within ___ when being assigned to variables.",
     answerChoices: [
@@ -70,7 +71,7 @@ var questions = [
       "all of the above"
     ],
     correctAnswer: "quotes"
-  }
+  },
   {
     questionText: "A very useful tool used during development and debugging for printing content to the debugger is:",
     answerChoices: [
@@ -81,90 +82,122 @@ var questions = [
     ],
     correctAnswer: "console log"
   }
-
+  
 ]
-
-
-//DATA
-//LOOK AT LESSON 5
-var strings1 = 'strings'
-var booleans1 = 'booleans'
-var alerts = 'alerts'
-var numbers = 'numbers'
-var quotes = 'quotes'
-var curlybrackets = 'curly-brackets'
-var parenthesis = 'parenthesis'
-var squarebrackets = 'sqaure-brackets'
-var comas = 'comas'
-var javascript = 'javascript'
-var terminalbash = 'terminal-bash'
-var forloops = 'for-loops'
-var counsellog = 'console-log'
-
-var wrongAnswer= -15
 var secondsLeft = 75;
 
-//FUNCTIONS
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// Deduct 15 seconds when question answered wrong
-for (var i = 0; i < secondsLeft.length; i--) {
-    ();
-   }
+function start(){
+  //start timer
+  timerInterval = setInterval(function () {
+              secondsLeft--;
+              secondsLeftEl.textContent = secondsLeft + " seconds left";
+          
+              if(secondsLeft === 0) {
+                // Stops execution of action at set interval
+                clearInterval(timerInterval);
+             
+              }
+          
+            }, 1000);
 
-var x = setInterval(function() {
+  //make container disappear
+containerEl.classList.add('hidden')
+  // make question container appear
+questionsEl.classList.remove('hidden')
+  // call question function
+  getQuestion()
+};
+
+function getQuestion(){
+//display question title
+titleEl.textContent = questions[0].questionText
+// create the buttons for the optionsEl
+
+
 }
 
-    function setTime() {
-        // Sets interval in variable
-        var timerInterval = setInterval(function () {
-          secondsLeft--;
-          secondsLeftEl.textContent = secondsLeft + " seconds left";
+startButton.addEventListener('click',start)
+
+// //DATA
+// //LOOK AT LESSON 5
+
+
+// var wrongAnswer= -15
+
+// //FUNCTIONS
+// // WHEN I answer a question incorrectly
+// // THEN time is subtracted from the clock
+// // Deduct 15 seconds when question answered wrong
+// for (var i = 0; i < secondsLeft.length; i--) {
+//     ();
+//    }
+
+// var x = setInterval(function() {
+// }
+
+//     function setTime() {
+//         // Sets interval in variable
+//         var timerInterval = setInterval(function () {
+//           secondsLeft--;
+//           secondsLeftEl.textContent = secondsLeft + " seconds left";
       
-          if(secondsLeft === 0) {
-            // Stops execution of action at set interval
-            clearInterval(timerInterval);
-            // Calls function to create and append image
-            sendMessage();
-          }
+//           if(secondsLeft === 0) {
+//             // Stops execution of action at set interval
+//             clearInterval(timerInterval);
+//             // Calls function to create and append image
+//             sendMessage();
+//           }
       
-        }, 1000);
-      }
-        //function load question function, that changes question text and answer choices  
+//         }, 1000);
+//       }
+//         //function load question function, that changes question text and answer choices  
 
-// Function to create and end quiz
-function sendMessage() {
+// // Function to create and end quiz
+// function sendMessage() {
   
-  }
+//   }
   
-  setTime();
-// INITIALIZATION
+//   setTime();
+// // INITIALIZATION
 
-variable.addEventListener("click", function() {
-    // If user clicks right answer
-    if (var === "right answer") {
-      var = "CORRECT!";
-    }
-    // If user clicks wrong answer 
-    else {
-      var = "INCORRECT!";
-    }
-  });
+// variable.addEventListener("click", function() {
+//     // If user clicks right answer
+//     if (var === "right answer") {
+//       var = "CORRECT!";
+//     }
+//     // If user clicks wrong answer 
+//     else {
+//       var = "INCORRECT!";
+//     }
+//   });
   
-// WHEN the game is over
-// THEN I can save my initials and score
+// // WHEN the game is over
+// // THEN I can save my initials and score
   
-var tagName = prompt("Please enter initals", "enter tag");
+// var tagName = prompt("Please enter initals", "enter tag");
 
-if (tagName !== "//INITIALS") {
-    alert("please enter valid initals");
-  } else {
-    // Creates element based on tag entered by user
-    var tag = document.createElement(tagName);
+// if (tagName !== "//INITIALS") {
+//     alert("please enter valid initals");
+//   } else {
+//     // Creates element based on tag entered by user
+//     var tag = document.createElement(tagName);
   
-    // Adds text content to created tag
-    tag.textContent = "This was made via prompts. It's a " + tagName + ".";
+//     // Adds text content to created tag
+//     tag.textContent = "This was made via prompts. It's a " + tagName + ".";
     
-    // Appends tag as child of document body
-    document.body.appendChild(tag);
-  }
+//     // Appends tag as child of document body
+//     document.body.appendChild(tag);
+//   }
+
+//   //ADD LOCAL STORAGE
+//    // TODO: Create user object from submission
+//    var userInitial = {
+//     Initials: firstNameInput.value,
+//     Score: score.value
+//   };
+
+//   // TODO: Set new submission to local storage 
+//   localStorage.setItem('userInital', JSON.stringify(userInput));
+//   renderMessage();
+  
+// });
