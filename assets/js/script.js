25a37480c39312d57f44a8ce365e8447b4380520
darvinmarte <119225399+darvinmarte@users.noreply.github.com>
2023-04-24 13:52:38 -0400

    added variables for Elements/IDS/CLASSES from HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,39 @@
 // WHEN all questions are answered or the timer reaches 0
 // THEN the game is over
 
+//Getting Elements/IDS/CLASSES from HTML
+var containerEl = document.getElementById("container");
+var questionsEl = document.getElementById("questions");
+var titleEl = document.getElementById("title");
+var optionsEl = document.getElementById("options");
+var messageEl = document.getElementById('message')
+
+// array of objects
+var questions = [
+{
+    questionText: "Commonly used data types DO NOT include ____.",
+    answerChoices: ["strings", "booleans", "alerts", "numbers"],
+    correctAnswer: "alerts",
+},
+{
+    questionText:"The condition in an if/else statement is enclosed within ____.",
+    answerChoices: ["quotes","curly-brackets","parenthesis","square-brackets",],
+    correctAnswer: "parenthesis",
+},
+{
+    questionText: "Arrays in Javascript can be used to store ___.",
+    answerChoices: ["numbers and strings","other arrays","booleans","all of the above",],
+    correctAnswer: "numbers and strings",
+},
+{
+questionText:"String values must be enclosed within ___ when being assigned to variables.",
+    answerChoices: ["commas", "curly brackets", "quotes", "all of the above"],
+    correctAnswer: "quotes",
+},
+{
+questionText:"A very useful tool used during development and debugging for printing content to the debugger is:",
+    answerChoices: ["javascript", "terminal/bash", "for loops", "console log"],
+    correctAnswer: "console log",
+},
+];
+
