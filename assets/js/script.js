dc9dfae2269eb7604c4cb7cfeefc0d1c6b54b63f
darvinmarte <119225399+darvinmarte@users.noreply.github.com>
2023-04-24 14:14:00 -0400

    altered CSS, added JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,41 +12,54 @@
 // THEN the game is over
 
 //Getting Elements/IDS/CLASSES from HTML
-var startButton = document.getElementById("start-quiz");
+var startButton = document.getElementById("startbutton");
 var secondsLeftEl = document.getElementById("secondsLeft");
 var containerEl = document.getElementById("container");
 var questionsEl = document.getElementById("questions");
 var titleEl = document.getElementById("title");
 var optionsEl = document.getElementById("options");
-var messageEl = document.getElementById('message')
+var messageEl = document.getElementById("message");
 
 // array of objects
 var questions = [
-{
+  {
     questionText: "Commonly used data types DO NOT include ____.",
     answerChoices: ["strings", "booleans", "alerts", "numbers"],
     correctAnswer: "alerts",
-},
-{
-    questionText:"The condition in an if/else statement is enclosed within ____.",
-    answerChoices: ["quotes","curly-brackets","parenthesis","square-brackets",],
+  },
+  {
+    questionText:
+      "The condition in an if/else statement is enclosed within ____.",
+    answerChoices: [
+      "quotes",
+      "curly-brackets",
+      "parenthesis",
+      "square-brackets",
+    ],
     correctAnswer: "parenthesis",
-},
-{
+  },
+  {
     questionText: "Arrays in Javascript can be used to store ___.",
-    answerChoices: ["numbers and strings","other arrays","booleans","all of the above",],
+    answerChoices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
     correctAnswer: "numbers and strings",
-},
-{
-questionText:"String values must be enclosed within ___ when being assigned to variables.",
+  },
+  {
+    questionText:
+      "String values must be enclosed within ___ when being assigned to variables.",
     answerChoices: ["commas", "curly brackets", "quotes", "all of the above"],
     correctAnswer: "quotes",
-},
-{
-questionText:"A very useful tool used during development and debugging for printing content to the debugger is:",
+  },
+  {
+    questionText:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
     answerChoices: ["javascript", "terminal/bash", "for loops", "console log"],
     correctAnswer: "console log",
-},
+  },
 ];
 
 var secondsLeft = 75;
@@ -54,25 +67,31 @@ var secondsLeft = 75;
 var questionNumber = 0;
 
 function start() {
-    //start timer
-    timerInterval = setInterval(function () {
+  //start timer
+  timerInterval = setInterval(function () {
     secondsLeft--;
     secondsLeftEl.textContent = secondsLeft + " seconds left";
     if (secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
     }
-    }, 1000);
+  }, 1000);
+  //make container disappear
+  containerEl.classList.add("hidden");
+  // make question container appear
+  questionsEl.classList.remove("hidden");
+  // call question function
+  getQuestion();
 }
 
 function getQuestion() {
-    //display question title
-    titleEl.textContent = questions[questionNumber].questionText;
-    // create the buttons for the optionsEl
-    for (let index = 1; index <= 4; index++) {
+  //display question title
+  titleEl.textContent = questions[questionNumber].questionText;
+  // create the buttons for the optionsEl
+  for (let index = 1; index <= 4; index++) {
     var button = document.querySelector("#answer" + index);
-      // forloop getting reference for each answer choice button and setting text content using question bank
+    // forloop getting reference for each answer choice button and setting text content using question bank
     button.textContent = questions[questionNumber].answerChoices[index - 1];
-      //going through the arrays in the answer choices
-    }
+    //going through the arrays in the answer choices
+  }
 }
